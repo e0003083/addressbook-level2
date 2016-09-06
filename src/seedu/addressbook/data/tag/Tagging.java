155199e36108dd3169c8067bc825a80107110f8a
Tan Jun Hao <tanjunhao.tjh@gmail.com>
2016-09-06 15:55:54 +0800

    Modified Tagging. Added ArrayList to store tagging.

--- src/seedu/addressbook/data/tag/Tagging.java
+++ src/seedu/addressbook/data/tag/Tagging.java
@@ -1,21 +1,30 @@
 package seedu.addressbook.data.tag;
 
+import java.util.ArrayList;
+
 import seedu.addressbook.data.person.Person;
 
 public class Tagging {
 	private String name;
 	private String tag;
 	private String addOrDelete;
+	public static ArrayList<String> allTaggings = new ArrayList<String>(); 
 	
-	public Tagging(Person person, Tag tag, String addOrDelete) {
+	public Tagging(Person person, Tag _tag, String _addOrDelete) {
 		this.name = person.getName().toString();
-		this.tag = tag.tagName;
-		this.addOrDelete = addOrDelete;		
+		this.tag = _tag.tagName;
+		this.addOrDelete = _addOrDelete;
+		allTaggings.add(addOrDelete + ' ' + name + ' ' + '[' + tag + ']');
 	}
 	
 	@Override
-	public String toString() {
-		return addOrDelete + ' ' + name + " [" + tag + ']';
+	public String toString(){
+		String output = "";
+		for(String taggings : allTaggings) {
+			output += taggings;
+		}
+		return output;
 	}
+	
 	
 } 
