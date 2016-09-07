33025d9033d14e251c40cf2b92e9aeeb9ab1755c
JunHao-PC\Jun Hao <tanjunhao.tjh@gmail.com>
2016-09-07 22:00:10 +0800

    Created Contact.java

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,28 @@
+package seedu.addressbook.data.person;
+
+public class Contact {
+    
+    public String value;
+    public boolean isPrivate;
+	
+	@Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Address // instanceof handles nulls
+                && this.value.equals(((Address) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
